7ed332f18aa9648ff0c417500aba76ddfcbfaa94
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-31 16:30:57 +0900

    added the Home component with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './styles/App.css'
+import './styles/App.css';
 import Nav from './components/Nav';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
+      <Home />
     </div>
   );
 }
