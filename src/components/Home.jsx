68d57a52dee76750af53d5ed73885e410e5363aa
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-09-01 11:47:17 +0900

    remove dummy pics for testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,6 @@ import '../styles/Home.css';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 import { Link } from 'react-router-dom';
-// import image1 from '../img/1.jpg';
-// import image2 from '../img/2.jpeg';
-// import image3 from '../img/3.jpg';
-// import image4 from '../img/4.jpg';
 import data from '../data/restaurants.json';
 
 export default function Home() {
@@ -25,14 +21,6 @@ export default function Home() {
                 <Link className="choice-no" to="/" style={linkStyle}>No</Link>
                 <Link className="choice-yes" to="/details" style={linkStyle}>Yes</Link>
             </div>
-            {/* <div className="picture-container">
-                <AliceCarousel autoPlay autoPlayInterval={3000} buttonsDisabled={true}>
-                    <img src={image1} className="sliderimg" alt="not loaded" />
-                    <img src={image2} className="sliderimg" alt="not loaded" />
-                    <img src={image3} className="sliderimg" alt="not loaded" />
-                    <img src={image4} className="sliderimg" alt="not loaded" />
-                </AliceCarousel>
-            </div> */}
             <AliceCarousel autoPlay autoPlayInterval={3000} buttonsDisabled={true} className="picture-container">
                 {images.map((image_url, index) => {
                     return (
