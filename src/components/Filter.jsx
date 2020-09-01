e52fbe049caf141125a23888a182fd746838284f
Eri Shimada <shimaeri515@gmail.com>
2020-09-01 13:49:52 +0900

    add filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Filter() {
+  return (
+    <div>
+      <div className="japanese">
+        <label>
+          <input
+            type="checkbox"></input>
+            Japanese
+        </label>
+      </div>
+      <div className="korean">
+        <label>
+          <input
+            type="checkbox"></input>
+            Korean
+        </label>
+      </div>
+      <div className="american">
+        <label>
+          <input
+            type="checkbox"></input>
+            American
+        </label>
+      </div>
+      <div className="mexican">
+        <label>
+          <input
+            type="checkbox"></input>
+            Mexican
+        </label>
+      </div>
+      <div className="chinese">
+        <label>
+          <input
+            type="checkbox"></input>
+            Chinese
+        </label>
+      </div>
+      <div className="japanese">
+        <label>
+          <input
+            type="checkbox"></input>
+        </label>
+      </div>
+    </div>
+  );
+}
