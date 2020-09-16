700f13439fe906c63458dc1d0ccc665c5a229c0b
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-09-16 17:43:06 +0900

    fix

--- server/index.js
+++ server/index.js
@@ -227,11 +227,11 @@ app.get("/dummyfavorites/:userid", async (req, res) => {
     const unswiped_rest = await dbRestCollection
       .find({ id: { $in: result } })
       .toArray();
-      const sCollection = await DbConnection.getCollection("Restaurants");
-      const sRestaurants = await sCollection.find().toArray();
-      const merged = new Set([...unswiped_rest, ...sRestaurants])
+      // const sCollection = await DbConnection.getCollection("Restaurants");
+      // const sRestaurants = await sCollection.find().toArray();
+      // const merged = new Set([...unswiped_rest, ...sRestaurants])
 
-    res.json(merged); 
+    res.json(unswiped_rest); 
     console.log(unswiped_rest.length);
   });
 });
